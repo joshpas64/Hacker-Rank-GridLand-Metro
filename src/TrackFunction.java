755ef25bb382d4b9cc5e04ee61788917c34d4685
Joshua Pascascio <joshua.pascascio@gmail.com>
2016-11-23 18:31:54 -0800

    Added a Main loop like Normal GUI's

--- src/TrackFunction.java
+++ src/TrackFunction.java
@@ -1,15 +1,39 @@
 import java.io.*;
 public class TrackFunction {
 	private Track[] tr;
-	int answer;
+	private int answer;
 	private int height,width,iter;
 	private int currentRun;
+	private BufferedReader bReader;
+	private StreamTokenizer stokens;
 	public TrackFunction() throws IOException
 	{
 		currentRun = 0;
-		answer = collectInput();
-		System.out.println(String.format("The number of spaces available for lamps in this grid of tracks is %d spaces",answer));
-		describeGrid();
+		answer = 0;
+	}
+	public void run() throws IOException
+	{
+		bReader = new BufferedReader(
+				new InputStreamReader(System.in));
+		stokens = new StreamTokenizer(bReader);
+		stokens.eolIsSignificant(true);
+		stokens.parseNumbers();
+		System.out.println("Welcome to the GridLand Metro Progam by super Novice joshpas! :D\n");
+		System.out.println("Do you want to start?\n");
+		String prompt = getContinue();
+		String newPrompt = prompt.replace(" ", "");
+		newPrompt = newPrompt.toLowerCase();
+		while(newPrompt.equals("yes"))
+		{
+			answer = collectInput();
+			System.out.println(String.format("The number of spaces available for lamps in this grid of tracks is %d spaces",answer));
+			describeGrid();
+			prompt = getContinue();
+			newPrompt = prompt.replace(" ", "");
+			newPrompt = newPrompt.toLowerCase();
+		}
+		bReader.close();
+		System.out.println("\nTHANKS FOR COMING BYE BYE :D :D :D");
 	}
 	private int runFunction(int row, int cols, int iterations) throws IOException
 	{
@@ -27,8 +51,7 @@ public class TrackFunction {
 	}
 	private int collectInput () throws IOException
 	{
-		String m = "Welcome to the GridLand Metro Progam by super Novice joshpas! :D\n";
-		m += "The first line of input will contain 3 numbers (integers PLEASE!)\n";
+		String m = "The first line of input will contain 3 numbers (integers PLEASE!)\n";
 		m += "The first number is the number of rows in the grid.\nThe second number is the number of columns un the grid.\n";
 		m += "The third is the number of tracks you are going to put in the grid\nThe following lines will be for the data of each respective track\n";
 		m += "The first number is the row number of the track\n";
@@ -38,11 +61,6 @@ public class TrackFunction {
 		m += "The rules are:\nThe rows and columns must be greater than zero.\nThe amount of tracks can be zero higher\n";
 		m += "You may start setting your grid and its tracks on the line below START HERE\n\nSTART HERE";
 		System.out.println(m);
-		BufferedReader bReader = new BufferedReader(
-		new InputStreamReader(System.in));
-		StreamTokenizer stokens = new StreamTokenizer(bReader);
-		stokens.eolIsSignificant(true);
-		stokens.parseNumbers();
 		int somVal,getToken,currentRow;
 		height = iter = width = -1;
 		getToken = stokens.nextToken();
@@ -76,7 +94,6 @@ public class TrackFunction {
 		}
 		if(iter == 0)
 		{
-			bReader.close();
 			return runFunction(height,width,0);
 		}
 		TrackRow[] tRows = new TrackRow[iter];
@@ -104,10 +121,9 @@ public class TrackFunction {
 			if( currentRow >= 0 && currentRow < height)
 				tr[currentRow].add(tRows[i]);
 		}
-		bReader.close();
 		return runFunction(height,width,iter);
 	}
-	public void describeGrid()
+	private void describeGrid()
 	{
 		String gridString = "";
 		gridString += String.format("\nThese are the result of execution number: %d\n\n", currentRun);
@@ -121,5 +137,13 @@ public class TrackFunction {
 		gridString += String.format("Also if you calculate the total number of tracks for each row it should equal the " +
 		"the third input of, %d, you entered\n", iter);
 		System.out.println(gridString);
+		System.out.println("\nWould you like to continue ?: ");
+	}
+	private String getContinue() throws IOException
+	{
+		if(currentRun > 0)
+			bReader.skip(1);
+		String prompt = bReader.readLine();
+		return prompt;
 	}
 };
